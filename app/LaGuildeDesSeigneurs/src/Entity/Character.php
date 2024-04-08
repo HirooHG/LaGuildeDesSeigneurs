a7f82ef2a80fe3599650019acaa2b3f5758ccc2c
HirooHG <hugo.golliet@etu.univ-smb.fr>
2024-04-08 14:57:21 +0200

    feat(api): added features & tests for character  - entity  - deps  - controller

---
+++ app/LaGuildeDesSeigneurs/src/Entity/Character.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+class Character
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $name = "Fëanturi";
+
+    #[ORM\Column(length: 50)]
+    private ?string $surname = "Fascination";
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $caste = "ElfeNoir";
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $knowledge = "Tourmenteuse";
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $intelligence = 160;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $strength = 1600;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $image = null;
+
+    public function toArray()
+    {
+        return get_object_vars($this);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): static
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getCaste(): ?string
+    {
+        return $this->caste;
+    }
+
+    public function setCaste(?string $caste): static
+    {
+        $this->caste = $caste;
+
+        return $this;
+    }
+
+    public function getKnowledge(): ?string
+    {
+        return $this->knowledge;
+    }
+
+    public function setKnowledge(?string $knowledge): static
+    {
+        $this->knowledge = $knowledge;
+
+        return $this;
+    }
+
+    public function getIntelligence(): ?int
+    {
+        return $this->intelligence;
+    }
+
+    public function setIntelligence(?int $intelligence): static
+    {
+        $this->intelligence = $intelligence;
+
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(?int $strength): static
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
