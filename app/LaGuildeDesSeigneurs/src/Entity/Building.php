0dedccfc0125a23710b9e65af951c4ec9f02ed2d
HirooHG <hugo.golliet.0209@gmail.com>
2024-06-08 15:46:10 +0200

    fix(HateOs): removed bdd call in HateOs

--- app/LaGuildeDesSeigneurs/src/Entity/Building.php
+++ app/LaGuildeDesSeigneurs/src/Entity/Building.php
@@ -10,236 +10,220 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BuildingRepository::class)]
-#[ORM\Table(name: '`building`')]
+#[ORM\Table(name: "`building`")]
 class Building
 {
-  #[ORM\Id]
-  #[ORM\GeneratedValue]
-  #[ORM\Column]
-  private ?int $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id;
 
-  #[ORM\Column(length: 20)]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 3,
-    max: 20,
-  )]
-  private ?string $name = "Lenora";
+    #[ORM\Column(length: 20)]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $name = "Lenora";
 
-  #[ORM\Column(length: 20)]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 3,
-    max: 20,
-  )]
-  private ?string $caste = "Guerrier";
+    #[ORM\Column(length: 20)]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $caste = "Guerrier";
 
-  #[ORM\Column]
-  #[Assert\PositiveOrZero]
-  private ?int $strength = 1000;
+    #[ORM\Column]
+    #[Assert\PositiveOrZero]
+    private ?int $strength = 1000;
 
-  #[ORM\Column(length: 50, nullable: true)]
-  #[Assert\Length(
-    min: 5,
-    max: 50,
-  )]
-  private ?string $image = "/buildings/lenora.webp";
+    #[ORM\Column(length: 50, nullable: true)]
+    #[Assert\Length(min: 5, max: 50)]
+    private ?string $image = "/buildings/lenora.webp";
 
-  #[ORM\Column(length: 20)]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 3,
-    max: 20,
-  )]
-  private ?string $slug = "chateau-lenora";
+    #[ORM\Column(length: 20)]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $slug = "chateau-lenora";
 
-  #[ORM\Column]
-  private ?int $rate = null;
+    #[ORM\Column]
+    private ?int $rate = null;
 
-  #[ORM\Column(length: 50)]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 40,
-    max: 40,
-  )]
-  private ?string $identifier = null;
+    #[ORM\Column(length: 50)]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 40, max: 40)]
+    private ?string $identifier = null;
 
-  #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-  private ?\DateTimeInterface $creation = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $creation = null;
 
-  #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-  private ?\DateTimeInterface $modification = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $modification = null;
 
-  /**
-   * @var Collection<int, Character>
-   */
-  #[ORM\OneToMany(targetEntity: Character::class, mappedBy: 'building')]
-  private Collection $characters;
+    /**
+     * @var Collection<int, Character>
+     */
+    #[ORM\OneToMany(targetEntity: Character::class, mappedBy: "building")]
+    private Collection $characters;
 
-  #[ORM\Column(type: Types::ARRAY)]
-  private array $_links = [];
+    private array $_links = [];
 
-  public function __construct()
-  {
-    $this->characters = new ArrayCollection();
-  }
-
-  public function getId(): ?int
-  {
-    return $this->id;
-  }
-
-  public function getName(): ?string
-  {
-    return $this->name;
-  }
-
-  public function setName(string $name): static
-  {
-    $this->name = $name;
-
-    return $this;
-  }
-
-  public function getCaste(): ?string
-  {
-    return $this->caste;
-  }
-
-  public function setCaste(string $caste): static
-  {
-    $this->caste = $caste;
-
-    return $this;
-  }
-
-  public function getStrength(): ?int
-  {
-    return $this->strength;
-  }
-
-  public function setStrength(int $strength): static
-  {
-    $this->strength = $strength;
-
-    return $this;
-  }
-
-  public function getImage(): ?string
-  {
-    return $this->image;
-  }
-
-  public function setImage(?string $image): static
-  {
-    $this->image = $image;
-
-    return $this;
-  }
-
-  public function getSlug(): ?string
-  {
-    return $this->slug;
-  }
-
-  public function setSlug(string $slug): static
-  {
-    $this->slug = $slug;
-
-    return $this;
-  }
-
-  public function getRate(): ?int
-  {
-    return $this->rate;
-  }
-
-  public function setRate(int $rate): static
-  {
-    $this->rate = $rate;
-
-    return $this;
-  }
-
-  public function getIdentifier(): ?string
-  {
-    return $this->identifier;
-  }
-
-  public function setIdentifier(string $identifier): static
-  {
-    $this->identifier = $identifier;
-
-    return $this;
-  }
-
-  public function getCreation(): ?\DateTimeInterface
-  {
-    return $this->creation;
-  }
-
-  public function setCreation(\DateTimeInterface $creation): static
-  {
-    $this->creation = $creation;
-
-    return $this;
-  }
-
-  public function getModification(): ?\DateTimeInterface
-  {
-    return $this->modification;
-  }
-
-  public function setModification(\DateTimeInterface $modification): static
-  {
-    $this->modification = $modification;
-
-    return $this;
-  }
-
-  /**
-   * @return Collection<int, Character>
-   */
-  public function getCharacters(): Collection
-  {
-    return $this->characters;
-  }
-
-  public function addCharacter(Character $character): static
-  {
-    if (!$this->characters->contains($character)) {
-      $this->characters->add($character);
-      $character->setBuilding($this);
+    public function __construct()
+    {
+        $this->characters = new ArrayCollection();
     }
 
-    return $this;
-  }
-
-  public function removeCharacter(Character $character): static
-  {
-    if ($this->characters->removeElement($character)) {
-      // set the owning side to null (unless already changed)
-      if ($character->getBuilding() === $this) {
-        $character->setBuilding(null);
-      }
+    public function getId(): ?int
+    {
+        return $this->id;
     }
 
-    return $this;
-  }
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
 
-  public function getLinks(): array
-  {
-      return $this->_links;
-  }
+    public function setName(string $name): static
+    {
+        $this->name = $name;
 
-  public function setLinks(array $_links): static
-  {
-      $this->_links = $_links;
+        return $this;
+    }
 
-      return $this;
-  }
+    public function getCaste(): ?string
+    {
+        return $this->caste;
+    }
+
+    public function setCaste(string $caste): static
+    {
+        $this->caste = $caste;
+
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(int $strength): static
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getRate(): ?int
+    {
+        return $this->rate;
+    }
+
+    public function setRate(int $rate): static
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getIdentifier(): ?string
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(string $identifier): static
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    public function getCreation(): ?\DateTimeInterface
+    {
+        return $this->creation;
+    }
+
+    public function setCreation(\DateTimeInterface $creation): static
+    {
+        $this->creation = $creation;
+
+        return $this;
+    }
+
+    public function getModification(): ?\DateTimeInterface
+    {
+        return $this->modification;
+    }
+
+    public function setModification(\DateTimeInterface $modification): static
+    {
+        $this->modification = $modification;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Character>
+     */
+    public function getCharacters(): Collection
+    {
+        return $this->characters;
+    }
+
+    public function addCharacter(Character $character): static
+    {
+        if (!$this->characters->contains($character)) {
+            $this->characters->add($character);
+            $character->setBuilding($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCharacter(Character $character): static
+    {
+        if ($this->characters->removeElement($character)) {
+            // set the owning side to null (unless already changed)
+            if ($character->getBuilding() === $this) {
+                $character->setBuilding(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getLinks(): array
+    {
+        return $this->_links;
+    }
+
+    public function setLinks(array $_links): static
+    {
+        $this->_links = $_links;
+
+        return $this;
+    }
 }
--- app/LaGuildeDesSeigneurs/src/Entity/Character.php
+++ app/LaGuildeDesSeigneurs/src/Entity/Character.php
@@ -8,271 +8,268 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CharacterRepository::class)]
-#[ORM\Table(name: '`character`')]
+#[ORM\Table(name: "`character`")]
 class Character
 {
-  #[ORM\Id]
-  #[ORM\GeneratedValue]
-  #[ORM\Column]
-  private ?int $id = null;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null; // si on veut une taille fixe il suffit
+    // de mettre la même valeur pour min et max
 
-  #[ORM\Column(length: 40, name: 'gls_identifier')]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 40, // si on veut une taille fixe il suffit
-    max: 40, // de mettre la même valeur pour min et max
-  )]
-  private ?string $identifier = null;
+    #[ORM\Column(length: 40, name: "gls_identifier")]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 40, max: 40)]
+    private ?string $identifier = null; // Pour que ce ne soit pas null
+    // Pour que ce ne soit pas blanc
+    //Définit une taille mini et maxi
+    // Messages pour customisation, sinon on peut les supprimer
 
-  #[ORM\Column(length: 20, name: 'gls_name')]
-  #[Assert\NotNull] // Pour que ce ne soit pas null
-  #[Assert\NotBlank] // Pour que ce ne soit pas blanc
-  #[Assert\Length( //Définit une taille mini et maxi
-    min: 3,
-    max: 20, // Messages pour customisation, sinon on peut les supprimer
-  )]
-  private ?string $name = "Fëanturi";
+    #[ORM\Column(length: 20, name: "gls_name")]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $name = "Fëanturi"; // 50 caractères
 
-  #[ORM\Column(length: 50, name: 'gls_surname')]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 3,
-    max: 50, // 50 caractères
-  )]
-  private ?string $surname = "Fascination";
+    #[ORM\Column(length: 50, name: "gls_surname")]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 50)]
+    private ?string $surname = "Fascination";
 
-  #[ORM\Column(length: 20, nullable: true, name: 'gls_caste')]
-  // Pas de NotNull et NotBlank puisque le champ est nullable
-  #[Assert\Length(
-    min: 3,
-    max: 20,
-  )]
-  private ?string $caste = "ElfeNoir";
+    // Pas de NotNull et NotBlank puisque le champ est nullable
+    #[ORM\Column(length: 20, nullable: true, name: "gls_caste")]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $caste = "ElfeNoir";
 
-  #[ORM\Column(length: 20, nullable: true, name: 'gls_knowledge')]
-  #[Assert\Length(
-    min: 3,
-    max: 20,
-  )]
-  private ?string $knowledge = "Arts";
+    #[ORM\Column(length: 20, nullable: true, name: "gls_knowledge")]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $knowledge = "Arts"; // OU #[Assert\Positive] si on ne veut pas de 0
 
-  #[ORM\Column(type: Types::SMALLINT, nullable: true, name: 'gls_intelligence')]
-  #[Assert\PositiveOrZero] // OU #[Assert\Positive] si on ne veut pas de 0
-  private ?int $intelligence = 160;
+    #[
+        ORM\Column(
+            type: Types::SMALLINT,
+            nullable: true,
+            name: "gls_intelligence"
+        )
+    ]
+    #[Assert\PositiveOrZero]
+    private ?int $intelligence = 160;
 
-  #[ORM\Column(type: Types::SMALLINT, nullable: true, name: 'gls_strength')]
-  #[Assert\PositiveOrZero]
-  private ?int $strength = 1600;
+    #[ORM\Column(type: Types::SMALLINT, nullable: true, name: "gls_strength")]
+    #[Assert\PositiveOrZero]
+    private ?int $strength = 1600;
 
-  #[ORM\Column(length: 50, nullable: true, name: 'gls_image')]
-  #[Assert\Length(
-    min: 5,
-    max: 50,
-  )]
-  private ?string $image = "/dame/bruh.webp";
+    #[ORM\Column(length: 50, nullable: true, name: "gls_image")]
+    #[Assert\Length(min: 5, max: 50)]
+    private ?string $image = "/dame/bruh.webp";
 
-  #[ORM\Column(length: 20, name: 'gls_slug')]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 3,
-    max: 20,
-  )]
-  private ?string $slug = "TourmenteursDuChaos";
+    #[ORM\Column(length: 20, name: "gls_slug")]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $slug = "TourmenteursDuChaos";
 
-  #[ORM\Column(length: 20, name: 'gls_kind')]
-  #[Assert\NotNull]
-  #[Assert\NotBlank]
-  #[Assert\Length(
-    min: 3,
-    max: 20,
-  )]
-  private ?string $kind = "Tourmenteuse";
+    #[ORM\Column(length: 20, name: "gls_kind")]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 20)]
+    private ?string $kind = "Tourmenteuse";
 
-  #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, name: 'gls_creation')]
-  private ?\DateTimeInterface $creation = null;
+    #[
+        ORM\Column(
+            type: Types::DATETIME_MUTABLE,
+            nullable: true,
+            name: "gls_creation"
+        )
+    ]
+    private ?\DateTimeInterface $creation = null;
 
-  #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, name: 'gls_modification')]
-  private ?\DateTimeInterface $modification = null;
+    #[
+        ORM\Column(
+            type: Types::DATETIME_MUTABLE,
+            nullable: true,
+            name: "gls_modification"
+        )
+    ]
+    private ?\DateTimeInterface $modification = null;
 
-  #[ORM\ManyToOne(inversedBy: 'characters')]
-  private ?Building $building = null;
+    #[ORM\ManyToOne(inversedBy: "characters")]
+    private ?Building $building = null;
 
-  #[ORM\Column(type: Types::ARRAY)]
-  private array $_links = [];
+    private array $_links = [];
 
-  public function getId(): ?int
-  {
-    return $this->id;
-  }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-  public function getName(): ?string
-  {
-    return $this->name;
-  }
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
 
-  public function setName(string $name): static
-  {
-    $this->name = $name;
+    public function setName(string $name): static
+    {
+        $this->name = $name;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getSurname(): ?string
-  {
-    return $this->surname;
-  }
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
 
-  public function setSurname(string $surname): static
-  {
-    $this->surname = $surname;
+    public function setSurname(string $surname): static
+    {
+        $this->surname = $surname;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getCaste(): ?string
-  {
-    return $this->caste;
-  }
+    public function getCaste(): ?string
+    {
+        return $this->caste;
+    }
 
-  public function setCaste(?string $caste): static
-  {
-    $this->caste = $caste;
+    public function setCaste(?string $caste): static
+    {
+        $this->caste = $caste;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getKnowledge(): ?string
-  {
-    return $this->knowledge;
-  }
+    public function getKnowledge(): ?string
+    {
+        return $this->knowledge;
+    }
 
-  public function setKnowledge(?string $knowledge): static
-  {
-    $this->knowledge = $knowledge;
+    public function setKnowledge(?string $knowledge): static
+    {
+        $this->knowledge = $knowledge;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getIntelligence(): ?int
-  {
-    return $this->intelligence;
-  }
+    public function getIntelligence(): ?int
+    {
+        return $this->intelligence;
+    }
 
-  public function setIntelligence(?int $intelligence): static
-  {
-    $this->intelligence = $intelligence;
+    public function setIntelligence(?int $intelligence): static
+    {
+        $this->intelligence = $intelligence;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getStrength(): ?int
-  {
-    return $this->strength;
-  }
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
 
-  public function setStrength(?int $strength): static
-  {
-    $this->strength = $strength;
+    public function setStrength(?int $strength): static
+    {
+        $this->strength = $strength;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getImage(): ?string
-  {
-    return $this->image;
-  }
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
 
-  public function setImage(?string $image): static
-  {
-    $this->image = $image;
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getSlug(): ?string
-  {
-    return $this->slug;
-  }
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
 
-  public function setSlug(string $slug): static
-  {
-    $this->slug = $slug;
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getKind(): ?string
-  {
-    return $this->kind;
-  }
+    public function getKind(): ?string
+    {
+        return $this->kind;
+    }
 
-  public function setKind(string $kind): static
-  {
-    $this->kind = $kind;
+    public function setKind(string $kind): static
+    {
+        $this->kind = $kind;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getCreation(): ?\DateTimeInterface
-  {
-    return $this->creation;
-  }
+    public function getCreation(): ?\DateTimeInterface
+    {
+        return $this->creation;
+    }
 
-  public function setCreation(\DateTimeInterface $creation): static
-  {
-    $this->creation = $creation;
+    public function setCreation(\DateTimeInterface $creation): static
+    {
+        $this->creation = $creation;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getIdentifier(): ?string
-  {
-    return $this->identifier;
-  }
+    public function getIdentifier(): ?string
+    {
+        return $this->identifier;
+    }
 
-  public function setIdentifier(string $identifier): static
-  {
-    $this->identifier = $identifier;
+    public function setIdentifier(string $identifier): static
+    {
+        $this->identifier = $identifier;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getModification(): ?\DateTimeInterface
-  {
-    return $this->modification;
-  }
+    public function getModification(): ?\DateTimeInterface
+    {
+        return $this->modification;
+    }
 
-  public function setModification(?\DateTimeInterface $modification): static
-  {
-    $this->modification = $modification;
+    public function setModification(?\DateTimeInterface $modification): static
+    {
+        $this->modification = $modification;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getBuilding(): ?Building
-  {
-    return $this->building;
-  }
+    public function getBuilding(): ?Building
+    {
+        return $this->building;
+    }
 
-  public function setBuilding(?Building $building): static
-  {
-    $this->building = $building;
+    public function setBuilding(?Building $building): static
+    {
+        $this->building = $building;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getLinks(): array
-  {
-      return $this->_links;
-  }
+    public function getLinks(): array
+    {
+        return $this->_links;
+    }
 
-  public function setLinks(array $_links): static
-  {
-      $this->_links = $_links;
+    public function setLinks(array $_links): static
+    {
+        $this->_links = $_links;
 
-      return $this;
-  }
+        return $this;
+    }
 }
